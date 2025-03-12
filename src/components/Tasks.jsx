bd24e402f76b3cf66f3fbd67953003d5b36f2a1a
Benedito-Dev <beneditobittencourtt@gmail.com>
2025-03-12 13:14:16 -0300

    wip

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import { ChevronsRightIcon, TrashIcon } from "lucide-react"
+import { useNavigate } from "react-router-dom"
 
 function Task(props) {
+    const navigate = useNavigate()
+
+    function onSeeDetailsClick(task) {
+        const query = new URLSearchParams();
+        query.set("title", task.title);
+        query.set("description", task.description);
+        navigate(`/task?${query.toString()}`);
+    }
+
     return (
         <ul className="space-y-4 p-6 bg-slate-200 shadow rounded-md">
             {props.tasks.map((task) => (
@@ -9,7 +19,7 @@ function Task(props) {
                 onClick={() => props.onTaskClick(task.id)}>
                     {task.title}
                 </button>
-                <button className="bg-slate-400  text-white p-2 rounded-md">
+                <button onClick={() => onSeeDetailsClick(task)} className="bg-slate-400  text-white p-2 rounded-md">
                     <ChevronsRightIcon />
                 </button>
                 <button onClick={() => props.onDeleteTaskClick(task.id)} className="bg-red-500  text-white p-2 rounded-md">
