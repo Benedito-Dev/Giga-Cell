9137052c357448ed74dc015ae2f2e34521f4125c
Benedito-Dev <beneditobittencourtt@gmail.com>
2025-03-12 00:48:20 -0300

    Adicionando novas Tasks

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react"
+
 function AddTask({ onAddTaskSubmit }) {
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
     return (
         <div className="grid space-y-4 p-6 mb-5 mt-5 bg-slate-200 shadow rounded-md">
-            <input className="bg-slate-50 outline-slate-400 text-sky-400 p-3 rounded-md shadow" type="text" name="title" id="" placeholder="Titulo da tarefa"/>
-            <input className="bg-slate-50 outline-slate-400 text-sky-400 p-3 rounded-md shadow" type="text" name="description" id="" placeholder="Descrição da tarefa"/>
-            <button onClick={onAddTaskSubmit()} className="bg-slate-400">Adicionar</button>
+            <input className="bg-slate-50 outline-slate-400 text-sky-400 p-3 rounded-md shadow" type="text" 
+            name="title" id="" placeholder="Titulo da tarefa" value={title} onChange={(event) =>setTitle(event.target.value)}/>
+            <input className="bg-slate-50 outline-slate-400 text-sky-400 p-3 rounded-md shadow" type="text" 
+            name="description" id="" placeholder="Descrição da tarefa" value={description} onChange={(event) =>setDescription(event.target.value)}/>
+            <button onClick={() => onAddTaskSubmit(title, description)} className="bg-slate-400">Adicionar</button>
         </div>
     )
 }
