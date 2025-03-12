b8f5bbc40356aa49c0ea6f67186428be86e76215
Benedito-Dev <beneditobittencourtt@gmail.com>
2025-03-12 14:03:29 -0300

    Giga-Cell

---
+++ src/components/NavBarr.jsx
@@ -0,0 +1,12 @@
+function NavBarr() {
+    return (
+        <nav className="nav-bar">
+            <ul className="nav-list">
+                <li>Home</li>
+                <li>Produtos</li>
+                <li>Carrinho</li>
+                <li>Sobre nós</li>
+            </ul>
+        </nav>
+    )
+}
