afeebaeeea207fa0f76a86a8d6433e1c49aab172
Benedito-Dev <beneditobittencourtt@gmail.com>
2025-03-28 18:52:44 -0300

    wip

--- Front-End/Giga Cell/src/App.jsx
+++ Front-End/Giga Cell/src/App.jsx
@@ -1,36 +1,6 @@
-// // import { useState } from 'react'
-// // import reactLogo from './assets/react.svg'
-// // import viteLogo from '/vite.svg'
-// // import ListaCelulares from './components/list'
-// import './App.css'
-// import NavBarr from './components/NavBarr'
-// import Banner from './pages/Banner'
-// import Products from './pages/Products'
-// import Footer from './pages/Footer'
-// import Acessorios from './pages/Acessorios'
-// import SubBarr from './components/SubBarr'
-// import Help from './components/Help'
-
-// function App() {
-//   // const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//     <NavBarr />
-//     <SubBarr />
-//     <Help />
-//     <Banner />
-//     <Products />
-//     <Acessorios />
-//     <Footer />
-//     </>
-//   )
-// }
-
-// export default App
-// App.jsx
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './views/Home';
+import Login from './views/Login';
 
 export default function App() {
   return (
@@ -38,6 +8,7 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="Login" element={<Login />} />
         {/* <Route path="/produtos" element={<Produtos />} />
         <Route path="/celulares" element={<Celulares />} />
         <Route path="/tablets" element={<Tablets />} />
---
+++ Front-End/Giga Cell/src/views/Login.jsx
@@ -0,0 +1,7 @@
+function Login() {
+    return (
+        <h1 className="text-black text-6xl font-bold" >Login</h1>
+    )
+}
+
+export default Login
