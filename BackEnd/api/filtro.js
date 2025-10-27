530e26d1ce8a9d14c7e6314ddfa7755cdc5e89d6
Benedito <beneditobittencourtt@gmail.com>
2025-10-27 17:04:37 -0300

    wip

--- BackEnd/api/filtro.js
+++ BackEnd/api/filtro.js
@@ -1,9 +1,4 @@
 export default function handler(req, res) {
-  // Headers CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
