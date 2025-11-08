e018cc227f3420abcc8c543ebea39bd59e7f19f2
Benedito <beneditobittencourtt@gmail.com>
2025-11-08 14:31:38 -0300

    wip

--- FrontEnd/src/api/api.js
+++ FrontEnd/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.DEV ? '/api' : 'https://giga-cell.vercel.app',
+  baseURL: 'http://localhost:3000',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
